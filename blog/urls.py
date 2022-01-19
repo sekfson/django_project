91075d2bbe2e55166a8e371b27c4022d914afe5c
sekfson <kouroumasekfson19@gmail.com>
2022-01-19 14:07:08 +0100

    utilisation des application

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('',views.home_view,name='home'),
     path('contact/',views.contact_view, name='contact'),
     path('blog/',views.article_view,name='article'),
+    path('users/',include('users.urls')),
 ]
